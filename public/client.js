6515bf965910fd6e0383e49b4b1fd75247adce4d
Antosik <grig_anton96@mail.ru>
2021-08-01 13:07:20 +0300

    feat: refactor to async/await

--- public/client.js
+++ public/client.js
@@ -1,16 +1,19 @@
+'use strict';
 
-
-$(document).ready(function() {
-
-	$(window).on('action:ajaxify.end', function(ev, data) {
+$(document).ready(function () {
+	$(window).on('action:ajaxify.end', function () {
 		$('#users-container, .pending-friends').on('click', '.friend-button', addFriendFunctionality);
 	});
 
-	$(window).on('action:ajaxify.contentLoaded', function(ev, data) {
+	$(window).on('action:ajaxify.contentLoaded', function (ev, data) {
 		if (data.tpl === 'account') {
 			var uid = $('[data-uid]').attr('data-uid');
 
-			socket.emit('plugins.friends.areFriendsOrRequested', {uid: uid}, function(err, isFriend) {
+			socket.emit('plugins.friends.areFriendsOrRequested', { uid: uid }, function (err, isFriend) {
+				if (err) {
+					console.error('[plugins/friends]', err);
+				}
+
 				if (isFriend[0]) {
 					$('#follow-btn').after(' <button class="btn btn-link btn-sm friend-button" data-uid="' + uid + '" data-type="unfriend">Remove Friend</button>');
 				} else {
@@ -29,23 +32,28 @@ $(document).ready(function() {
 
 	function friendCommand(type, uid, btn) {
 		socket.emit('plugins.friends.' + type, {
-			uid: uid
-		}, function(err) {
+			uid: uid,
+		}, function (err) {
 			if (err) {
 				return app.alertError(err.message);
 			}
 
-			if (type === 'friend') {
-				btn.attr('data-type', '').prop('disabled', true).html('Request Sent');
-			} else if (type === 'unfriend') {
-				btn.attr('data-type', 'friend')
-					.addClass('btn-warning').removeClass('btn-link').html('Add Friend');
-			} else if (type === 'accept') {
-				ajaxify.go('user/' + app.user.userslug + '/friends');
-			} else if (type === 'reject') {
-				btn.parents('.registered-user').remove();
+			switch (type) {
+				case 'friend':
+					btn.attr('data-type', '').prop('disabled', true).html('Request Sent');
+					break;
+				case 'unfriend':
+					btn.attr('data-type', 'friend')
+						.addClass('btn-warning').removeClass('btn-link').html('Add Friend');
+					break;
+				case 'accept':
+					ajaxify.go('user/' + app.user.userslug + '/friends');
+					break;
+				case 'reject':
+					btn.parents('.registered-user').remove();
+					break;
 			}
 		});
 		return false;
 	}
-})
+});
